0fde18fd090f4313d7d9325b990fe77cc1e0c5e2
Ramon Navarro Bosch <ramon.nb@gmail.com>
2020-02-07 12:34:35 +0100

    Adding wait_for

--- run.py
+++ run.py
@@ -2,33 +2,58 @@ import requests
 import yaml
 import os
 
-config = os.environ.get('CONFIG_FILE', 'config.yaml')
-
-if os.path.exists(config):
-    with open(config, 'r') as config_file:
-        yaml_configuration = yaml.load(config_file.read())
-else:
-    raise AttributeError(f'Not existing config file {config}')
-
-for config_element in yaml_configuration.get('sequencial', []):
-    method = config_element.get('method', 'get').lower()
+def get_operation(method):
+    method = method.lower()
     if hasattr(requests, method): 
         operation = getattr(requests, method)
     else:
         raise AttributeError(f'Not existing method file {method}')
+    return operation
 
-    url = config_element.get('url', None)
+def do_call(configuration):
+    method = configuration.get('method', 'get')
+    operation = get_operation(method)
+
+    url = configuration.get('url', None)
     if url is None:
         raise AttributeError('No url defined')
 
     options = {}
-    options['params'] = config_element.get('params', None)
-    options['headers'] = config_element.get('headers', None)
-    options['data'] = config_element.get('data', None)
-    options['auth'] = config_element.get('auth', None)
+    options['params'] = configuration.get('params', None)
+    options['headers'] = configuration.get('headers', None)
+    options['data'] = configuration.get('data', None)
+    options['auth'] = configuration.get('auth', None)
 
     print(f"Calling: {method} {url}")
     response = operation(url, **options)
+    return response
+
+
+config = os.environ.get('CONFIG_FILE', 'config.yaml')
+
+if os.path.exists(config):
+    with open(config, 'r') as config_file:
+        yaml_configuration = yaml.load(config_file.read(), Loader=yaml.SafeLoader)
+else:
+    raise AttributeError(f'Not existing config file {config}')
+
+# WAIT FOR
+wait_for = yaml_configuration.get('wait_for')
+
+if wait_for is not None:
+    response = do_call(wait_for)
+    expected_status = wait_for.get('status')
+    intents = wait_for.get('intents')
+    wait_time = wait_for.get('wait')
+
+    if response.status_code != expected_status and intents > 0:
+        time.sleep(wait_time)
+        response = do_call(wait_for)
+        intents -= 1
+
+
+for config_element in yaml_configuration.get('sequencial', []):
+    response = do_call(config_element)
 
     response.raise_for_status()
     print("######## RESPONSE #########")
